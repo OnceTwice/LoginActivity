e62d39b9364208be5105f7a94ad007f19db7ac9b
cho <you-smile@nate.com>
2016-11-29 19:58:54 +0900

    1

--- app/src/main/java/com/bit2016/loginactivity/MainActivity.java
+++ app/src/main/java/com/bit2016/loginactivity/MainActivity.java
@@ -2,6 +2,9 @@ package com.bit2016.loginactivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // html string UI에 적용하기
+        TextView textView = (TextView)findViewById( R.id.tv_signup );
+        textView.setMovementMethod( LinkMovementMethod.getInstance() );
+        textView.setText( Html.fromHtml( getString(R.string.login_url)));
     }
 }
